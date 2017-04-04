67e46937aea926b09ee928876ef8477d1105c69e
Aman Kapur <akapur07@gmail.com>
2017-04-05 02:24:07 +0530

    Hero Model added and Home page

---
+++ TourOfHeroes/src/app/Model/hero.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+    id: Number;
+    name: string;
+}
--- TourOfHeroes/src/app/app.component.ts
+++ TourOfHeroes/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Hero }from './Model/hero.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of heroes!';
+
+  hero: Hero = {
+    id: 1,
+    name : 'Windstorm'
+  }
 }
